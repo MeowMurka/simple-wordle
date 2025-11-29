ef0a8e12dac86e07a82aad01918510717ea187b6
MeowMurka <danya.rayushkin@mail.ru>
2025-11-29 13:37:28 +0500

    Update build-last-update.sh V1.1

--- build-last-update.sh
+++ build-last-update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Генерируем lastUpdate.js с текущей датой сборки
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo "const lastUpdate = new Date('$DATE');" > lastUpdate.js
-echo "const info = document.createElement('div'); info.textContent = \`Последнее изменение: \${lastUpdate.toLocaleString()}\`; info.className = 'last-update'; document.body.appendChild(info);" >> lastUpdate.js
+echo "const info = document.createElement('div');" >> lastUpdate.js
+echo "info.textContent = \`Последнее изменение: \${lastUpdate.toLocaleString()}\`;" >> lastUpdate.js
+echo "info.className = 'last-update';" >> lastUpdate.js
+echo "document.body.appendChild(info);" >> lastUpdate.js
